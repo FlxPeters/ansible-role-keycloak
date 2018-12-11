39b817d35565c136386c24000dd7e2f581a565ec
Felix Peters <peters@develop4edu.de>
2018-12-11 21:20:21 +0100

    Add basic tests and make idempotent

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,36 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_keycloak_user(host):
+    u = host.user('keycloak')
+    assert u.exists
+    assert u.group == 'keycloak'
 
+
+def test_keycloak_group(host):
+    g = host.group('keycloak')
+    assert g.exists
+
+
+def test_keycloak_base_direcory_exists(host):
+    f = host.file('/opt/keycloak')
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.user == 'keycloak'
+    assert f.group == 'keycloak'
+
+
+def test_keycloak_current_symlink(host):
+    f = host.file('/opt/keycloak/current')
+    assert f.exists
+    assert f.is_symlink
+
+
+def test_keycloak_is_a_service(host):
+    service = host.service('keycloak')
+    assert service.is_running
+    assert service.is_enabled
+
+
+def test_keycloak_listen_on_8080(host):
+    socket = host.socket('tcp://0.0.0.0:8080')
+    socket.is_listening
